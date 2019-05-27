4470a3e5cdc6e1e7a0c024d0abd7e5c9ea99294b
RenSalazar <salazarrenelie@gmail.com>
2019-05-28 00:01:29 +0800

    Coded TagLayerUtil and TargetDetector scripts

---
+++ Assets/Scripts/Character/TagLayerUtil.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class TagLayerUtil {
+
+	public static bool IsUntagged(string tag) {
+		return tag.Equals(GameTags.Untagged.ToString());
+	}
+
+	public static bool IsTagPlayer(string tag) {
+		return (tag.Equals(GameTags.Player.ToString()) || 
+			tag.Equals(GameTags.PlayerShielded.ToString()));
+	}
+
+	public static bool IsLayerInteractor(int layer) {
+		return (layer == LayerMask.NameToLayer (GameLayers.Interactor.ToString()));
+	}
+
+}
+
+
+//NOTE: Declare in this enum all the TAGS you declared in Unity Editor.
+/*
+	UnityEditorInternal.InternalEditorUtility.tags would be a great tool to use instead of this class.
+	But APIs under UnityEditorInternal are not officially supported and might be deleted at some point. So, I won't really rely on them working forever.
+*/
+public enum GameTags {
+
+	Untagged,
+
+	Projectile,
+
+	NPC,
+
+	Enemy,
+
+	Player, 
+	PlayerShielded
+
+}
+
+public enum GameLayers {
+
+	Default,
+
+	Interactor
+
+}
---
+++ Assets/Scripts/Character/TargetDetector.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+namespace Character {
+	
+	[RequireComponent(typeof(Collider2D))]
+	public class TargetDetector : MonoBehaviour {
+
+		[SerializeField] private GameTags[] m_targetTags;
+
+		[Tooltip("If set to FALSE, this will capture ALL targets within its range upon detection." +
+			"If set to TRUE, disregard the value of Range.")]
+		[SerializeField] private bool m_isLockedToOne;
+
+		[Range(3f, 100f)]
+		[SerializeField] private float m_range = 5f;
+
+		public ReactiveProperty<bool> m_isTargetDetected { get; private set; }
+		public List<Collider2D> m_targets { get; private set; }
+
+		private void Awake() {
+			m_isTargetDetected = new ReactiveProperty<bool>(false);
+			m_targets = new List<Collider2D>();
+		}
+
+		private void Start() {
+			this.OnCollisionEnter2DAsObservable()
+				.Where(otherCollision2D => IsMatchingTag(otherCollision2D.gameObject.tag))
+				.Subscribe(otherCollision2D => {
+					if(m_isTargetDetected.Value == false) {
+						RefreshTargets(otherCollision2D.collider);
+					}
+				})
+				.AddTo(this);
+		}
+
+		private void RefreshTargets(Collider2D detectedCollider) {
+			m_targets.Clear();
+			bool hasNewTargets = false;
+
+			if(m_isLockedToOne && IsMatchingTag(detectedCollider.tag)) {				
+				m_targets.Add(detectedCollider);
+				hasNewTargets = true;
+			}
+			else {
+				Collider2D[] tempTargets = Physics2D.OverlapCircleAll(transform.position, m_range);	
+
+				//filter targets by tags
+				foreach(Collider2D collider2D in tempTargets) {
+					if(IsMatchingTag(collider2D.tag)) {
+						m_targets.Add(collider2D);
+						hasNewTargets = true;
+					}	
+				}
+			}
+				
+			m_isTargetDetected.Value = hasNewTargets;
+		}
+
+		private bool IsMatchingTag(string tag) {
+			for(int x=0; x<m_targetTags.Length; x++) {
+				if(tag.Equals(m_targetTags[x].ToString())) {
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+	}
+
+}
+	
