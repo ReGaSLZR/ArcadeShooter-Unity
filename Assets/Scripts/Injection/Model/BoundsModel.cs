29b33763d1c28c957d217ec7080dcf0c169f80d9
RenSalazar <salazarrenelie@gmail.com>
2019-05-28 16:35:38 +0800

    Coded ModelsInstaller and BoundsModel for holding the instance and method for getting Vector2 points within a given bound

---
+++ Assets/Scripts/Injection/Model/BoundsModel.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Injection.Model {
+
+    public class BoundsModel : MonoBehaviour, BoundsModel.IGetter
+    {
+
+        [SerializeField] private Collider m_colliderMovement;
+
+        public interface IGetter {
+            Vector2 GetRandomPosition();
+        }
+
+        private void Awake() {
+            if(m_colliderMovement == null) {
+                LogUtil.PrintError(this, this.GetType(), "Cannot have NULL collider.");
+                Destroy(this);
+            }
+        }
+
+        public Vector2 GetRandomPosition() {
+            Bounds bounds = m_colliderMovement.bounds;
+            return new Vector2(Random.Range(bounds.min.x, bounds.max.x),
+                Random.Range(bounds.min.y, bounds.max.y));
+        }
+    }
+
+}
---
+++ Assets/Scripts/Injection/ModelsInstaller.cs
@@ -0,0 +1,17 @@
+﻿using Injection.Model;
+using UnityEngine;
+using Zenject;
+
+namespace Injection {
+
+    public class ModelsInstaller : MonoInstaller<ModelsInstaller> {
+
+        [SerializeField] private BoundsModel m_boundsModel;
+
+        public override void InstallBindings() {
+            Container.Bind<BoundsModel.IGetter>().FromInstance(m_boundsModel);
+        }
+
+    }
+
+}
